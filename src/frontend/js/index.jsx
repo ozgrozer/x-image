fc6b66e0cf303627c959fd4c998a3bf8edef10c9
Ozgur <ozgr@live.com>
2021-01-31 23:52:02 +0300

    Show loading text

--- src/frontend/js/index.jsx
+++ src/frontend/js/index.jsx
@@ -16,32 +16,52 @@ const validations = {
 
 const App = () => {
   const [imageData, setImageData] = useState()
-  const postSubmit = res => setImageData(res.data)
+
+  const [formIsSubmitting, setFormIsSubmitting] = useState(false)
+  const onSubmit = res => {
+    if (res.isFormValid) {
+      setImageData('')
+      setFormIsSubmitting(true)
+    }
+  }
+  const postSubmit = res => {
+    setFormIsSubmitting(false)
+    setImageData(res.data)
+  }
 
   return (
     <div>
       <Form
+        onSubmit={onSubmit}
         postSubmit={postSubmit}
         postOptions={{ method: 'post', url: '/get-image' }}
       >
-        <Input
-          type='text'
-          name='tweetUrl'
-          validations={validations.empty}
-          placeholder='https://twitter.com/ozgrozer/status/1355138534777245697'
-        />
+        <fieldset disabled={formIsSubmitting}>
+          <Input
+            type='text'
+            name='tweetUrl'
+            validations={validations.empty}
+            placeholder='https://twitter.com/ozgrozer/status/1355138534777245697'
+          />
 
-        <div className='generatedImageWrapper'>
-          {
-            imageData
-              ? (
-                <img src={`data:image/png;base64, ${imageData}`} className='generatedImage' />
-                )
-              : (
-                <div className='helpText'>Type the tweet URL above</div>
-                )
-          }
-        </div>
+          <div className='generatedImageWrapper'>
+            {
+              imageData
+                ? (
+                  <img src={`data:image/png;base64, ${imageData}`} className='generatedImage' />
+                  )
+                : (
+                  <div className='helpText'>
+                    {
+                      formIsSubmitting
+                        ? (<div>Loading...</div>)
+                        : (<div>Type the tweet URL above</div>)
+                    }
+                  </div>
+                  )
+            }
+          </div>
+        </fieldset>
       </Form>
     </div>
   )
