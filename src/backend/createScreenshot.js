79e55f4777d41aee0584ec85b2a04e571604830f
Ozgur <ozgr@live.com>
2021-01-31 18:43:46 +0300

    Add create screenshot function

---
+++ src/backend/createScreenshot.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer')
+
+const createScreenshot = async props => {
+  try {
+    const { lang, width, theme, hideCard, hideThread, tweetId } = props
+
+    const browser = await puppeteer.launch({ headless: true })
+    const page = await browser.newPage()
+    await page.goto(`https://platform.twitter.com/embed/index.html?dnt=true&embedId=twitter-widget-0&frame=false&hideCard=${hideCard}&hideThread=${hideThread}&id=${tweetId}&lang=${lang}&theme=${theme}&widgetsVersion=ed20a2b%3A1601588405575`, { waitUntil: 'networkidle0' })
+
+    const embedDefaultWidth = 550
+    const percent = width / embedDefaultWidth
+    const pageWidth = embedDefaultWidth * percent
+    const pageHeight = 100
+    await page.setViewport({ width: pageWidth, height: pageHeight })
+
+    const propsForPage = { theme: theme, percent: percent }
+    await page.evaluate(props => {
+      const { theme, percent } = props
+      const body = document.querySelector('body')
+      body.style.backgroundColor = theme === 'dark' ? '#000' : '#fff'
+      body.style.zoom = `${100 * percent}%`
+      const articleWrapper = document.querySelector('#app > div')
+      articleWrapper.style.border = 'none'
+    }, (propsForPage))
+    await page.screenshot({ fullPage: true, path: `capture-${lang}-${theme}-${width}-${tweetId}.png` })
+
+    await browser.close()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+module.exports = createScreenshot
--- src/backend/server.js
+++ src/backend/server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const port = 1270
 
+const createScreenshot = require('./createScreenshot')
+
 app.use(express.static(path.join(__dirname, '..', '..', 'dist')))
 
 app.listen(port, () => {
@@ -11,4 +13,17 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'dist', 'index.html'))
+})
+
+app.post('/get-image', (req, res) => {
+  createScreenshot({
+    lang: 'en',
+    width: 1000,
+    theme: 'dark',
+    hideCard: 'false',
+    hideThread: 'false',
+    tweetId: '1355138534777245697'
+  })
+
+  res.json({ success: true })
 })
