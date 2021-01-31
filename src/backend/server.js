515fa342f9734e3652b2b02f930f5af89c0a150b
Ozgur <ozgr@live.com>
2021-01-31 19:31:04 +0300

    Get the passed env var or use manual port

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const port = 1270
+const port = process.env.PORT || 1270
 
 const createScreenshot = require('./createScreenshot')
 
