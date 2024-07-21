305410149ceca2588fae57c5ad87706997c0ef5d
mac <ozgr@live.com>
2024-07-22 00:54:35 +0300

    Try to work on Vercel

--- src/backend/createScreenshot.js
+++ src/backend/createScreenshot.js
@@ -1,13 +1,26 @@
 const puppeteer = require('puppeteer')
+const puppeteerCore = require('puppeteer-core')
+const chromium = require('@sparticuz/chromium')
 
 const createScreenshot = async props => {
   try {
     const { lang, width, theme, padding, hideCard, hideThread, tweetId } = props
 
-    const browser = await puppeteer.launch({
-      headless: 'new',
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    })
+    let browser
+    if (process.env.VERCEL_ENV === 'production') {
+      const executablePath = await chromium.executablePath()
+      browser = await puppeteerCore.launch({
+        executablePath,
+        args: chromium.args,
+        headless: chromium.headless,
+        defaultViewport: chromium.defaultViewport
+      })
+    } else {
+      browser = await puppeteer.launch({
+        headless: 'new',
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      })
+    }
 
     const page = await browser.newPage()
     await page.goto(`https://platform.twitter.com/embed/index.html?dnt=true&embedId=twitter-widget-0&frame=false&hideCard=${hideCard}&hideThread=${hideThread}&id=${tweetId}&lang=${lang}&theme=${theme}&widgetsVersion=ed20a2b%3A1601588405575`, { waitUntil: 'networkidle0' })
